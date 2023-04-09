2291868c2549894b8de8b5be6d2d900dbdc28810
Tev-J <tj_delph@yahoo.com>
2023-04-08 20:42:34 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(array) {
+  let rval;
+  if (array.find((rec) => rec.result === "W")) {
+    rval = array.find((rec) => rec.result === "W").year;
+  }
+  return rval;
+}
